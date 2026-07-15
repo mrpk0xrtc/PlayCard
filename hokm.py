e5c68387ac8fa464b98438faf45139a4dd5f251a
nimi <nima.2413.iran@gmail.com>
2026-07-15 12:42:22 +0330

    define functions

--- hokm.py
+++ hokm.py
@@ -7,31 +7,44 @@ class Hokm:
     def __init__(self):
         self.deck = cards.Deck()
         self.deck.shuffle()
-        self.palyer1 = cards.player("1"self.give_hand(13))
-        self.player2 = cards.player("2",self.give_hand(13))
-        self.player3 = cards.player("3",self.give_hand(13))
-        self.player4 = cards.player("4", self.give_hand(13))
+        self.palyer1 = cards.Player("1",self.give_hand(13))
+        self.player2 = cards.Player("2",self.give_hand(13))
+        self.player3 = cards.Player("3",self.give_hand(13))
+        self.player4 = cards.Player("4", self.give_hand(13))
         self.playerlist = [self.palyer1, self.player2, self.player3, self.player4]
         self.hakem = random.choice(self.playerlist)
-        self.score=[]
-        self.score=[0]=0
-        self.score=[1]=1
-
-
-        
-
-        c = 1
-        for i in self.hakem:
-              if c <= 5:
-                print(i)
-                c += 1
+        self.raundscore=[0,0]
+        self.gamescore=[0,0]
+        self.teamwinner=str()
 
         self.hokm = input("1.diamonds\n 2.hearts\n 3.spades\n 4.clubs\nEnter hokm:")
         cards.Card.trump_suit = self.hokm
 
     def Run(self):
         while True:
-            self.game()
+            self.game(self.hakem)
+            if self.raundscore[0]==7:
+                self.gamescore[0]+=1
+                self.raundscore[0]=0
+                self.raundscore[1]=0
+                tmp_n=int(input("if do you to countinue enter '1' else enter '2':"))
+                if tmp_n==2 :
+                    if self.gamescore[0]>self.gamescore[1]:
+                        print("winter team is team 1")
+                    elif self.gamescore[1]>self.gamescore[0]:
+                        print("winter team is team 2")
+                    break
+            elif self.roundscore[1]==7:
+                self.gamescore[1]+=1
+                self.raundscore[0]=0
+                self.raundscore[1]=0
+                tmp_n=int(input("if do you to countinue enter '1' else enter '2':"))
+                if tmp_n==2:
+                    if self.gamescore[0]>self.gamescore[1]:
+                        print("winter team is team 1")
+                    elif self.gamescore[1]>self.gamescore[0]:
+                        print("winter team is team 2")
+                    break
 
     def game(self,winner):
             temp_n = int(winner.id)
@@ -48,10 +61,19 @@ class Hokm:
             winner_card = max(tmp_dict[1],tmp_dict[2],tmp_dict[3],tmp_dict[4])
             winner(winner_card.id)
     def winner(self,winnercard):
-        if winnercard in ["0","2"]:
-            self.scores[0] +=1
         if winnercard in ["1","3"]:
-            self.scores[1] +=1
+            self.roundscores[0] +=1
+            if self.hakem in [self.palyer1,self.player3]:
+                pass
+            else :
+                self.hakem=random.choice([self.palyer1,self.player3])
+
+        if winnercard in ["2","4"]:
+            self.roundscores[1] +=1
+            if self.hakem in [self.player2,self.player4]:
+                pass
+            else :
+                self.hakem=random.choice([self.player2,self.player4])
 
             
 
