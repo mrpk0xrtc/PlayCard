0bf6fe9bb69214ab64ca18d224c7df55c3bd890e
nimi <nima.2413.iran@gmail.com>
2026-07-15 11:16:18 +0330

    work on function game

--- hokm.py
+++ hokm.py
@@ -5,17 +5,14 @@ class Hokm:
     def __init__(self):
         self.deck = cards.Deck()
         self.deck.shuffle()
-        self.palyer1 = cards.player(give_hand(13))
-        self.player2 = cards.player(give_hand(13))
-        self.player3 = cards.player(give_hand(13))
-        self.player4 = cards.player(give_hand(13))
+        self.palyer1 = cards.player("1"self.give_hand(13))
+        self.player2 = cards.player("2",self.give_hand(13))
+        self.player3 = cards.player("3",self.give_hand(13))
+        self.player4 = cards.player("4", self.give_hand(13))
         self.playerlist = [self.palyer1, self.player2, self.player3, self.player4]
         self.hakem = random.choice(self.playerlist)
 
-        if self.hakem % 4 == 1: self.hakem = self.player1
-        elif self.hakem % 4 == 2: self.hakem = self.player2
-        elif self.hakem % 4 == 3: self.hakem = self.player3 
-        elif self.hakem % 4 == 0: self.hakem = self.player4
+        
 
         c = 1
         for i in self.hakem:
@@ -26,16 +23,23 @@ class Hokm:
         self.hokm = input("1.diamonds\n 2.hearts\n 3.spades\n 4.clubs\nEnter hokm:")
 
     def game(self,winner):
-         while True:
-             temp_n=0
-             for i in self.playerlist:
-                 if winner==i: break
-                 temp_n+=1
-             for i in range(1,4):
-                  self.playerlist[temp_n].pop()
-                  temp_n+=1
-                  if temp_n%4==0:
-                       temp_n=0
+            temp_n=0
+            for i in self.playerlist:
+                if winner==i: break
+                temp_n+=1
+            for i in range(1,4):
+                print(self.playerlist[temp_n])
+                temp_card=input("Enter your card")
+                t = temp_card.split()
+                temp=self.playerlist[temp_n].draw(int(t[0]),t[1])
+                tmp_dict = {}
+                tmp_dict[temp_n]={temp}
+                temp_n+=1
+                if temp_n%4==0:
+                    temp_n=0
+                    
+             
+             
 
     def give_hand(self, n):
         tmp = []
